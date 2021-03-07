f4f430ab6b2ddfa0d3e777364a35bfb464a59125
Luis Adrian Morales Guzman <intrusionz3r0@kali>
2021-03-06 20:16:01 -0600

    Empleados Insertar,ConsultaGeneral,Eliminar

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-import os
+import os,random,string
 from flask import Flask,render_template,request,redirect,url_for,abort
 from flask_mysqldb import MySQL
 from flask_sqlalchemy import SQLAlchemy
 from modelo.models import Empleados,Usuarios,Turnos
 from werkzeug.utils import secure_filename
 from flask_login import LoginManager, login_user, current_user, logout_user, login_required
+from sqlalchemy import create_engine
 
 
 
@@ -12,7 +13,9 @@ app = Flask(__name__)
 app.secret_key = "s3cr3t"
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://admin:admin@localhost/ERP'
+engine = create_engine('mysql://admin:admin@localhost/ERP')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['UPLOAD_FOLDER'] = "static/uploads/"
 loginManager=LoginManager()
 loginManager.init_app(app)
 loginManager.login_view="login"
@@ -34,7 +37,7 @@ def login():
 def iniciarSesion():
     Us=Usuarios()
     Us=Us.validar(request.form['username'],request.form['pass'])
-    if(Us!=None):
+    if(Us!=None and Usuarios.is_active(Us)):
         login_user(Us)
         return render_template("home.html")
     else:
@@ -80,7 +83,7 @@ def ventanaEliminarTurno(id):
     except:
         return "No se puede eliminar"
 
-    return render_template('Turnos/registrarTurnos.html')
+    return redirect(url_for('ventanaOpcionesTurno'))
 
 
 @app.route('/insertarTurnosBD', methods=['POST'])
@@ -107,14 +110,98 @@ def actualzarTurnosBD():
 
 
 
-   
-
 #Apartado de Adrian
 
 @app.route('/registrarEmpleado')
+@login_required
 def ventanaRegistrarEmpleado():
    return render_template("Empleados/registrarEmpleado.html")
 
+@app.route('/editarEmpleado/<int:id>')
+@login_required
+def ventanaEditarEmpleado(id):
+    
+    emp = Empleados()
+    emp.id_empleado=id
+    empleados = emp.consultaIndividual()
+
+    usr= Usuarios()
+    usr.id_usuario=empleados.id_usuario
+    usuarios=usr.consultaIndividual()
+    
+    return render_template("Empleados/modificarEmpleado.html",usuarios=usuarios,empleados=empleados)
+
+
+   
+
+@app.route('/opcionesEmpleados')
+@login_required
+def ventanaOpcionesEmpleados():
+    usr = Usuarios()
+    emp = Empleados()
+    usuarios=usr.consultaGeneral()
+    empleados = emp.consultaGeneral()
+    return render_template('Empleados/opcionesEmpleados.html',usuarios=usuarios,empleados=empleados)
+   
+
+@app.route('/eliminarEmpleado/<int:id>')
+def eliminarEmpleado(id):
+    usr=Usuarios()
+    usr.id_usuario=id
+    datos=usr.consultaIndividual()
+    
+    ids=datos.id_usuario
+    with engine.connect() as connection:
+        result = connection.execute("update Usuarios set estatus_usuario='Inactivo' where id_usuario={};".format(ids))
+    
+    return redirect(url_for('ventanaOpcionesEmpleados'))
+   
+
+
+@app.route('/insertarEmpleado', methods=['POST'])
+def registrarEmpleadoBD():
+    usr = Usuarios()
+    emp = Empleados()
+    usr.nombre=request.form['nombre']
+    usr.apellido_paterno=request.form['apaterno']
+    usr.apellido_materno=request.form['amaterno']
+    usr.genero=request.form['genero']
+    emp.tipo =request.form['tusuario'] 
+    emp.salario_diario=request.form['sdiario']
+    emp.fecha_contracion=request.form['fcontratacion']
+    emp.nss=request.form['nss']
+    usr.fecha_nacimiento=request.form['fnacimiento']
+    usr.fecha_registro=request.form['fregistro']
+    usr.correo=request.form['correo']
+    usr.telefono=request.form['telefono']
+    emp.dias_vacaciones=request.form['dvacaciones']
+    emp.dias_permiso=request.form['dpermiso']
+    usr.colonia=request.form['colonia']
+    usr.calle=request.form['calle']
+    usr.numero_casa=request.form['ncasa']
+    usr.usuario=request.form['usuario']
+    usr.passwd=request.form['pass1']
+    usr.estatus_usuario="Activo"
+    RFC= usr.apellido_paterno[:2]+usr.apellido_materno[:1]+usr.nombre[:1]+str(usr.fecha_nacimiento.split("-")[0][2:])+str(usr.fecha_nacimiento.split("-")[1])+usr.fecha_nacimiento.split("-")[2]+random.choice(string.ascii_letters)+str(random.randrange(10))+random.choice(string.ascii_letters)
+    emp.rfc=RFC.upper()
+    
+
+    foto=request.files['file']
+    os.mkdir("static/uploads/"+emp.rfc)
+    filename1 = secure_filename(foto.filename)
+    path = os.path.join(app.config['UPLOAD_FOLDER']+emp.rfc, foto.filename)
+    foto.save(path)
+    emp.foto=filename1
+
+    usr.insertar()
+
+    with engine.connect() as connection:
+        result = connection.execute("SELECT * FROM Usuarios ORDER by id_usuario DESC LIMIT 1;")
+        for row in result:
+            emp.id_usuario=row['id_usuario']
+    
+    emp.insertar()
+    return redirect(url_for('ventanaRegistrarEmpleado'))
 
 
 
