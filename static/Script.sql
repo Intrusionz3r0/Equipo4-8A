e27560d3411a231784807f3a006275d1256440ef
KarenPonce98 <67848137+KarenPonce98@users.noreply.github.com>
2021-04-15 23:06:02 -0500

    corrección DB

--- static/Script.sql
+++ static/Script.sql
@@ -40,9 +40,7 @@ CREATE TABLE  IF NOT EXISTS Materia(
     id_materia int(11) not null auto_increment,
     nombre varchar(50) not null,
     total_unidades int(11) not null,
-    id_empleado int(11) not null,
-    primary key(id_materia),
-    foreign key(id_Empleado) references Empleados(id_empleado)
+    primary key(id_materia)
 );
 
 CREATE TABLE  IF NOT EXISTS Turnos(
@@ -58,11 +56,14 @@ CREATE TABLE IF NOT EXISTS Grupos(
     id_grupo int (11) not null auto_increment,
     grado varchar(4) not null,
     grupo varchar(4) not null,
+    capacidad int(5) not null,
     id_turno int(11) not null,
     id_materia int(11) not null,
+    id_empleado int(11) not null,
     primary key(id_grupo),
     foreign key(id_turno) references Turnos(id_turno),
-    foreign key(id_materia) references Materia(id_materia)
+    foreign key(id_materia) references Materia(id_materia),
+    foreign key(id_empleado) references Empleados(id_empleado)
 );
 
 CREATE TABLE IF NOT EXISTS Alumnos(
@@ -113,8 +114,8 @@ insert into Usuarios values(2, "Chris", "Evans", "Rodrigez", "Masculino", "Mar",
 insert into Turnos values(1, "Matutino", "07:00", "12:00", "Activo");
 insert into Edificios values(1, "Principal", "Administracion","Contiene 5 aulas con capacidd de 100 alumnos", "Habilitado");
 insert into Empleados values(1, 1,"MUMM990308M0H", 2500,'2017-03-8', 1701165,15,3,"elon.jpg");
-insert into Materia values(1,"ERP", 5,1);
-insert into Grupos values(1,"8","A",1,1);
+insert into Materia values(1,"ERP", 5);
+insert into Grupos values(1,"3","A",10,1,1,1);
 insert into Aulas values(1,1,"AE34j",100,"Activo");
 insert into Alumnos values(1,2,1,"CHRISVAS1998","evans.jpg");
 insert into Calificacion values(1,1,1,10.0,1,"SI");
@@ -143,170 +144,93 @@ CREATE TABLE IF NOT EXISTS Horario(
 	primary key(id_horario)
 );
 
-CREATE TABLE IF NOT exists Proveedor(
-
-	id_proveedor int(11) not null  auto_increment,
-
-	nombre varchar(50) not null,	
-
-	telefono varchar(12) not null,
-
-	direccion varchar(200) not null,
-
-	contacto varchar(65) not null,
-
-        correo varchar(80) not null,
-
-        primary key(id_proveedor)
-
-	
-
-);
 
 CREATE TABLE IF NOT exists Productos(
-
 	id_producto int (11) not null  auto_increment,
-
 	nombre varchar(50) not null,
+	descripcion varchar(50) not null,
+	costo_unitario float not null,
 
-	tipo varchar(30) not null,
-
-	categoria varchar(50) not null,
-
-        costoU float not null,
-
-	id_proveedores int(11) not null,
-
-    
-
-    primary key(id_producto),
-
-    foreign key(id_proveedores) references Proveedor(id_proveedor)
-
+    primary key(id_producto)
 );
 
-CREATE TABLE  IF NOT EXISTS Compras(
-
-    id_Compra int(11) not null auto_increment,
-
-    tipo varchar(65) not null,
-
-    id_producto int(11) not null,
-
-    costoT float not null,
-
-    cantidad int not null,
-
+CREATE TABLE IF NOT exists Proveedor(
+	id_proveedor int(11) not null  auto_increment,
+	nombre varchar(50) not null,	
+	telefono varchar(12) not null,
+	direccion varchar(200) not null,
+	contacto varchar(65) not null,
+	correo varchar(80) not null,
     
+	primary key(id_proveedor)
+);
 
-    primary key(id_Compra),
+CREATE TABLE  IF NOT EXISTS Pedidos(
+    id_pedido int(11) not null auto_increment,
+    id_proveedor int(11) not null,
+    id_producto int(11) not null,
+    fecha_emitido date not null,
+    fecha_entregaEstimada date not null,
+    precio_total float not null,
+    descripcion varchar(80) not null,
+    cantidad int not null,
+    forma_pago varchar(50) not null,
+    lugar_entrega varchar(50) not null,
 
-    foreign key(id_producto) references Productos(id_producto)
-
+    primary key(id_pedido),
+    foreign key(id_proveedor) references Proveedor(id_proveedor),
+	foreign key(id_producto) references Productos(id_producto)
 );
 
 CREATE TABLE  IF NOT EXISTS Nomina(
-
     id_nomina int (11) not null auto_increment,
-
+    fecha_elaboracion date not null,
     id_empleado int(11) not null,
-
-    periodo_inicio date not null,
-
-    periodo_fin date not null,
-
-    bonos float not null,
-
-    impuesto float not null,
-
+    fecha_pago date not null,
+    subtotal float not null,
+    descripcion_retencion varchar(75)not null,
+    importe_retencion float not null,
     pago_total float not null,
-
-
-
+    descripcion_bonos varchar(75) not null,
+    importe_bonos float not null,
+    forma_pago varchar(50) not null,
+    estatus varchar(25) not null,
+    
     primary key(id_nomina),
-
     foreign key(id_empleado) references Empleados(id_empleado)
-
 );
 
 CREATE TABLE IF NOT EXISTS Pagos(
-
     id_pagos int(11) not null  auto_increment,
-
     descripcion varchar(200) not null,
-
     tipo varchar (59)not null,
-
     id_usuario int(11) not null,
-
     monto float not null,
-
     
-
     primary key(id_pagos),
-
 	foreign key(id_usuario) references Usuarios(id_usuario)
-
 );
 
 CREATE table IF NOT EXISTS Asistencias(
-
     id_asistencia int (11) not null  auto_increment,
-
     id_empleado int(11) not null,
-
     llegada datetime not null,
-
     salida datetime not null,
 
-    
-
     primary key(id_asistencia),
-
     foreign key(id_empleado) references Empleados(id_empleado)
-
 ); 
 
-CREATE TABLE  IF NOT EXISTS DocumentosAlumnos(
-
+CREATE TABLE  IF NOT EXISTS Documentos(
     id_documento int(11) not null  auto_increment,
-
     nombre varchar(50) not null,
-
     descripcion varchar(60) not null,
-
     archivo varchar(200) not null,
-
-    id_alumno int(11) not null,
+    id_usuario int(11) not null,
 
     primary key(id_documento),
-
-    foreign key(id_alumno) references Alumnos(id_alumno)
-
-
-
+    foreign key(id_usuario) references Usuarios(id_usuario)
 );
-
-CREATE TABLE  IF NOT EXISTS DocumentosEmpleados(
-
-    id_documento int(11) not null  auto_increment,
-
-    nombre varchar(50) not null,
-
-    descripcion varchar(60) not null,
-
-    archivo varchar(200) not null,
-
-    id_empleado int(11) not null,
-
-
-
-    primary key(id_documento),
-
-    foreign key(id_empleado) references Empleados(id_empleado)
-
-);
-
 #Contar Tablas
 SELECT COUNT(*) from Information_Schema.Tables where TABLE_TYPE = 'BASE TABLE' and table_schema = 'ERP';
 
