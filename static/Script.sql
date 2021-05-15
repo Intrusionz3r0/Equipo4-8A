42100917c2204480436cfd3c760f397f2219ee64
Luis Adrian Morales Guzman <intrusionz3r0@kali>
2021-05-15 18:58:33 -0500

    Chingo de Empleados Test

--- static/Script.sql
+++ static/Script.sql
@@ -624,7 +624,7 @@ insert into Usuarios values(13, "alumno12", "alumno12", "alumno12", "Masculino",
 insert into Alumnos values(12,13,1,"CHRISVAS1998","evans.jpg");
 
 
-
+/*
 insert into Usuarios values(14,"Docente2", "Docente2", "Docente2", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente2@add.com",355115233,"Docente2","admin","Docente","Activo");
 insert into Usuarios values(15,"Docente3", "Docente3", "Docente3", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente3add.com",355115233,"Docente3","admin","Docente","Activo");
 insert into Usuarios values(16, "Docente4", "Docente4", "Docente4", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente4@add.com",355115233,"Docente4","admin","Docente","Activo");
@@ -636,6 +636,55 @@ insert into Usuarios values(21, "Docente9", "Docente9", "Docente9", "Masculino",
 insert into Usuarios values(22, "Docente10", "Docente10", "Docente10", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente10@add.com",355115233,"Docente10","admin","Docente","Activo");
 insert into Usuarios values(23, "Docente11", "Docente11", "Docente11", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docent11@add.com",355115233,"Docente11","admin","Docente","Activo");
 insert into Usuarios values(24, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente12","admin","Docente","Activo");
+insert into Usuarios values(25, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente13","admin","Docente","Activo");
+insert into Usuarios values(26, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente14","admin","Docente","Activo");
+insert into Usuarios values(27, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente16","admin","Docente","Activo");
+insert into Usuarios values(28, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente126565","admin","Docente","Activo");
+insert into Usuarios values(29, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente1254","admin","Docente","Activo");
+insert into Usuarios values(30, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente1215","admin","Docente","Activo");
+insert into Usuarios values(31, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente18","admin","Docente","Activo");
+insert into Usuarios values(32, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente19","admin","Docente","Activo");
+insert into Usuarios values(33, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente20","admin","Docente","Activo");
+insert into Usuarios values(34, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente21","admin","Docente","Activo");
+insert into Usuarios values(35, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente22","admin","Docente","Activo");
+insert into Usuarios values(36, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente23","admin","Docente","Activo");
+insert into Usuarios values(37, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente24","admin","Docente","Activo");
+insert into Usuarios values(38, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente25","admin","Docente","Activo");
+insert into Usuarios values(39, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente26","admin","Docente","Activo");
+insert into Usuarios values(40, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente27","admin","Docente","Activo");
+insert into Usuarios values(41, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente28","admin","Docente","Activo");
+insert into Usuarios values(42, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente29","admin","Docente","Activo");
+insert into Usuarios values(43, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente30","admin","Docente","Activo");
+insert into Usuarios values(44, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente31","admin","Docente","Activo");
+insert into Usuarios values(45, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente32","admin","Docente","Activo");
+insert into Usuarios values(46, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente33","admin","Docente","Activo");
+insert into Usuarios values(47, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente34","admin","Docente","Activo");
+insert into Usuarios values(48, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente35","admin","Docente","Activo");
+insert into Usuarios values(49, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente36","admin","Docente","Activo");
+insert into Usuarios values(50, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente37","admin","Docente","Activo");
+insert into Usuarios values(51, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente38","admin","Docente","Activo");
+insert into Usuarios values(52, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente834","admin","Docente","Activo");
+insert into Usuarios values(53, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente40","admin","Docente","Activo");
+insert into Usuarios values(54, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente141","admin","Docente","Activo");
+insert into Usuarios values(55, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente42","admin","Docente","Activo");
+insert into Usuarios values(56, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente43","admin","Docente","Activo");
+insert into Usuarios values(256, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente44","admin","Docente","Activo");
+insert into Usuarios values(58, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente45","admin","Docente","Activo");
+insert into Usuarios values(59, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente46","admin","Docente","Activo");
+insert into Usuarios values(60, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente47","admin","Docente","Activo");
+insert into Usuarios values(61, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente48","admin","Docente","Activo");
+insert into Usuarios values(62, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente49","admin","Docente","Activo");
+insert into Usuarios values(63, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente50","admin","Docente","Activo");
+insert into Usuarios values(65, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente51","admin","Docente","Activo");
+insert into Usuarios values(66, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente52","admin","Docente","Activo");
+insert into Usuarios values(67, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente53","admin","Docente","Activo");
+insert into Usuarios values(68, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente155","admin","Docente","Activo");
+insert into Usuarios values(69, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente156","admin","Docente","Activo");
+insert into Usuarios values(70, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente157","admin","Docente","Activo");
+insert into Usuarios values(71, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente1258","admin","Docente","Activo");
+insert into Usuarios values(72, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente1259","admin","Docente","Activo");
+insert into Usuarios values(73, "Docente12", "Docente12", "Docente12", "Masculino", "Mar", "Arriaga", 51, '1999-03-8','2020-03-3', "Docente12@add.com",355115233,"Docente80","admin","Docente","Activo");
+*/
 
 
 insert into Grupos values(6,"3","A",10,1,1,1,"Activo");
